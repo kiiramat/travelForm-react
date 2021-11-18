e4b223c105e2ac3bc3d0cdcf1480a52573efa1ee
kiiramat <kiiramati@gmail.com>
2021-11-18 15:53:50 +0000

    first name, last name and age  implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import React from 'react';
+import InputForm from './components/InputForm';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-
+      firstName: "",
+      lastName: "",
+      age: ""
     }
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(name, value) {
+    this.setState({
+      [name] : value 
+    })
   }
 
   render() {
@@ -14,12 +24,32 @@ class App extends React.Component {
       <div className="App">
         <main>
           <form>
+            <InputForm 
+              name = "firstName"
+              value = {this.state.firstName}
+              placeholder = "First Name"
+              handleChange = {this.onChange} 
+            />
+            <InputForm 
+              name = "lastName"
+              value = {this.state.lastName}
+              placeholder = "Last Name"
+              handleChange = {this.onChange} 
+            />
+            <InputForm 
+              name = "age"
+              value = {this.state.age}
+              placeholder = "Age"
+              handleChange = {this.onChange} 
+            />
 
 
 
+
+{/* 
             <input placeholder="First Name" /><br />
             <input placeholder="Last Name" /><br />
-            <input placeholder="Age" /><br />
+            <input placeholder="Age" /><br /> */}
             
             {/* Create radio buttons for gender here */}
             <br />
@@ -34,8 +64,8 @@ class App extends React.Component {
           </form>
           <hr />
           <h2>Entered information:</h2>
-          <p>Your name: {/* First and last name here */}</p>
-          <p>Your age: {/* Age here */}</p>
+          <p>Your name: {this.state.firstName} {this.state.lastName}</p>
+          <p>Your age: {this.state.age}</p>
           <p>Your gender: {/* Gender here */}</p>
           <p>Your destination: {/* Destination here */}</p>
           <p>
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,15 +1,29 @@
 import React from "react";
 
 class InputForm extends React.Component {
-    constructor() {
-        super()
-    }
+  constructor() {
+    super()
+    this.onInputChange = this.onInputChange.bind(this)
+  }
 
-    render() {
-        return (
-            <h1>hi!</h1>
-        )
-    }
+  onInputChange(event) {
+    const { name, value } = event.target
+    this.props.handleChange(name, value)
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          name={this.props.name}
+          placeholder={this.props.placeholder}
+          onChange={this.onInputChange}
+          autoComplete="off"
+        />
+      </div>
+    )
+  }
 }
 
 export default InputForm
