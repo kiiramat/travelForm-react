1b7f28f256fc52f8b371c0f19f09daeb246a112c
kiiramat <kiiramati@gmail.com>
2021-11-18 16:02:17 +0000

    chosen gender shows in 'entered information' section

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends React.Component {
           <h2>Entered information:</h2>
           <p>Your name: {this.state.firstName} {this.state.lastName}</p>
           <p>Your age: {this.state.age}</p>
-          <p>Your gender: {/* Gender here */}</p>
+          <p>Your gender: {this.state.gender}</p>
           <p>Your destination: {/* Destination here */}</p>
           <p>
               Your dietary restrictions: 
